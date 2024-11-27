8aae0b2e5d78bbe22bf415e0d4dc038eb9e93ba7
Dt8333 <lb0016@foxmail.com>
2024-11-27 11:47:18 +0800

    Make Patch compatible with SDK 5-r.1

--- src/SDKv4/frameworkpatch.ts
+++ src/SDKv4/frameworkpatch.ts
@@ -1,36 +1,33 @@
-import { Live2DCubismFramework as cubismmodelsettingjson } from "@framework/cubismmodelsettingjson";
+import { CubismModelSettingJson } from "@framework/cubismmodelsettingjson";
 import { Live2DCubismFramework as cubismjson } from '@framework/utils/cubismjson';
 import Value = cubismjson.Value;
 
 declare module "@framework/icubismmodelsetting" {
-    export namespace Live2DCubismFramework {
-         interface ICubismModelSetting {
-            /**
-             * 得到动作音频延迟
-             * @param groupName   动作组名
-             * @param index   动作索引
-             */
-            getMotionSoundDelay(groupName: string, index: number): number;
-        }
+    export interface ICubismModelSetting {
+        /**
+         * 得到动作音频延迟
+         * @param groupName   动作组名
+         * @param index   动作索引
+         */
+        getMotionSoundDelay(groupName: string, index: number): number;
     }
+
 }
 
+const SoundDelay = "SoundDelay";
 declare module "@framework/cubismmodelsettingjson" {
-    export namespace Live2DCubismFramework {
-        const SoundDelay = "SoundDelay";
-        interface CubismModelSettingJson {
-            /**
-             * 得到动作音频延迟
-             * @param groupName   动作组名
-             * @param index   动作索引
-             */
-            getMotionSoundDelay(groupName: string, index: number): number;
-            isExistMotionSoundDelay(groupName: string, index: number): boolean;
-        }
+    export interface CubismModelSettingJson {
+        /**
+         * 得到动作音频延迟
+         * @param groupName   动作组名
+         * @param index   动作索引
+         */
+        getMotionSoundDelay(groupName: string, index: number): number;
+        isExistMotionSoundDelay(groupName: string, index: number): boolean;
     }
 }
 
-cubismmodelsettingjson.CubismModelSettingJson.prototype.getMotionSoundDelay = function (
+CubismModelSettingJson.prototype.getMotionSoundDelay = function (
     groupName: string,
     index: number
 ): number {
@@ -39,10 +36,10 @@ cubismmodelsettingjson.CubismModelSettingJson.prototype.getMotionSoundDelay = fu
     }
 
     return this._jsonValue
-        .at(2) //FrequestNode.FrequestNode_Motions Framework 4
+        .at(2) //FrequestNode.FrequestNode_Motions Framework 5
         .getValueByString(groupName)
         .getValueByIndex(index)
-        .getValueByString(cubismmodelsettingjson.SoundDelay)
+        .getValueByString(SoundDelay)
         .toInt();
 };
 
@@ -52,14 +49,14 @@ cubismmodelsettingjson.CubismModelSettingJson.prototype.getMotionSoundDelay = fu
  * @param index 动作索引
  * @private
  */
-cubismmodelsettingjson.CubismModelSettingJson.prototype.isExistMotionSoundDelay = function (
+CubismModelSettingJson.prototype.isExistMotionSoundDelay = function (
     groupName: string,
     index: number
 ): boolean {
     const node: Value = this._jsonValue
-        .at(2) //FrequestNode.FrequestNode_Motions Framework 4
+        .at(2) //FrequestNode.FrequestNode_Motions Framework 5
         .getValueByString(groupName)
         .getValueByIndex(index)
-        .getValueByString(cubismmodelsettingjson.SoundDelay);
+        .getValueByString(SoundDelay);
     return !node.isNull() && !node.isError();
 };
